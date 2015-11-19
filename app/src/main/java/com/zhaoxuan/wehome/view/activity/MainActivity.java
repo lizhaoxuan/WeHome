417111ae71f620eabe047f8066f15d2734028922
李兆轩 <690770333@qq.com>
2015-11-19 21:30:55 +0800

    1：增加应用启动动画 2：邀请家人模块

--- app/src/main/java/com/zhaoxuan/wehome/view/activity/MainActivity.java
+++ app/src/main/java/com/zhaoxuan/wehome/view/activity/MainActivity.java
@@ -1,25 +1,59 @@
 package com.zhaoxuan.wehome.view.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.zhaoxuan.wehome.R;
-import com.zhaoxuan.wehome.framework.baseclass.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity {
+
+    @Bind(R.id.loadImg)
+    protected ImageView loadImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        startActivity(new Intent(this, LoginActivity.class));
-        this.finish();
-
+        initView();
     }
 
-    @Override
-    protected void initView() {
 
+    private void initView() {
+        //透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.main_loading);
+        loadImg.startAnimation(animation);
+
+
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                MainActivity.this.finish();
+                overridePendingTransition(R.anim.activity_enter,R.anim.activity_exit);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }

ad79d44bc55ef2c8fa1777a8f991e5b2d6e76ff4
李兆轩 <690770333@qq.com>
2015-12-23 20:53:13 +0800

    no message

---
+++ app/src/main/java/com/zhaoxuan/wehome/view/activity/FamilyActivity.java
@@ -0,0 +1,20 @@
+package com.zhaoxuan.wehome.view.activity;
+
+import android.os.Bundle;
+
+import com.zhaoxuan.wehome.R;
+import com.zhaoxuan.wehome.framework.base.BaseActivity;
+
+public class FamilyActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family);
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
---
+++ app/src/main/java/com/zhaoxuan/wehome/view/activity/WZoneActivity.java
@@ -0,0 +1,20 @@
+package com.zhaoxuan.wehome.view.activity;
+
+import android.os.Bundle;
+
+import com.zhaoxuan.wehome.R;
+import com.zhaoxuan.wehome.framework.base.BaseActivity;
+
+public class WZoneActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wzone);
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}

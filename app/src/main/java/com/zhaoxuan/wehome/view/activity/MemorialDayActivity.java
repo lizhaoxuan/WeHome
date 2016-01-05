51d7e7d3e9c746305f0e48c2ac490b24691bc905
李兆轩 <690770333@qq.com>
2016-01-05 20:35:49 +0800

    纪念日页面透明化完成

--- app/src/main/java/com/zhaoxuan/wehome/view/activity/MemorialDayActivity.java
+++ app/src/main/java/com/zhaoxuan/wehome/view/activity/MemorialDayActivity.java
@@ -1,8 +1,8 @@
 package com.zhaoxuan.wehome.view.activity;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
@@ -10,66 +10,109 @@ import android.renderscript.Allocation;
 import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
-import android.view.View;
-import android.widget.ImageView;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.TextView;
 
 import com.zhaoxuan.wehome.R;
 import com.zhaoxuan.wehome.framework.base.BaseActivity;
 
+import butterknife.Bind;
+
 public class MemorialDayActivity extends BaseActivity {
+
+    @Bind(R.id.contentLayout)
+    protected ViewGroup contentLayout;
+    @Bind(R.id.titleLayout)
+    protected ViewGroup titleLayout;
+    @Bind(R.id.familyTitleText)
+    protected TextView familyTitleText;
+    @Bind(R.id.familyDayText)
+    protected TextView familyDayText;
+    @Bind(R.id.familyLabelText)
+    protected TextView familyLabelText;
+    @Bind(R.id.wehomeTitleText)
+    protected TextView wehomeTitleText;
+    @Bind(R.id.wehomeDayText)
+    protected TextView wehomeDayText;
+    @Bind(R.id.wehomeLabelText)
+    protected TextView wehomeLabelText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memorial_day);
 
-        ImageView view  = (ImageView)findViewById(R.id.view);
-
-        Bitmap sentBitmap= BitmapFactory.decodeResource(getResources(),R.drawable.bg_memorial);
-
-        blur(sentBitmap,view);
 
     }
 
 
     @Override
     protected void initView() {
-
+        applyBlur();
     }
 
-    private void blur(Bitmap bkg, View view) {
+    private void updateView() {
+        familyTitleText.setText("我们的家已经：");
+        familyDayText.setText("125");
+        familyLabelText.setText("天");
+        wehomeTitleText.setText("微家创建已经");
+        wehomeDayText.setText("56");
+        wehomeLabelText.setText("天");
+    }
+
+    private void applyBlur() {
+        contentLayout.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                contentLayout.getViewTreeObserver().removeOnPreDrawListener(this);
+                contentLayout.buildDrawingCache();
+
+                Bitmap bmp = contentLayout.getDrawingCache();
+                blur(bmp, titleLayout);
+                return true;
+            }
+        });
+    }
+
+    private void blur(Bitmap bkg, ViewGroup view) {
         long startMs = System.currentTimeMillis();
+        float scaleFactor = 1;
+        float radius = 15;
 
-        float radius = 20;
-
-        Bitmap overlay = Bitmap.createBitmap((int) (200),
-                (int) (200), Bitmap.Config.ARGB_8888);
+        Bitmap overlay = Bitmap.createBitmap((int) (view.getMeasuredWidth() / scaleFactor),
+                (int) (view.getMeasuredHeight() / scaleFactor), Bitmap.Config.ARGB_8888);
 
         Canvas canvas = new Canvas(overlay);
+        canvas.translate(-view.getLeft() / scaleFactor, -view.getTop() / scaleFactor);
+        canvas.scale(1 / scaleFactor, 1 / scaleFactor);
+        Paint paint = new Paint();
+        paint.setFlags(Paint.FILTER_BITMAP_FLAG);
+        canvas.drawBitmap(bkg, 0, 0, paint);
 
-        canvas.translate(-view.getLeft(), -view.getTop());
-        canvas.drawBitmap(bkg, 0, 0, null);
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+        canvas.drawRect(view.getLeft(), view.getTop(), view.getRight(), view.getBottom(), paint);
+
 
         RenderScript rs = RenderScript.create(this);
-
         Allocation overlayAlloc = Allocation.createFromBitmap(
                 rs, overlay);
-
         ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(
                 rs, Element.U8_4(rs));
-
         blur.setInput(overlayAlloc);
-
         blur.setRadius(radius);
-
         blur.forEach(overlayAlloc);
-
         overlayAlloc.copyTo(overlay);
-
-        view.setBackground(new BitmapDrawable(
-                getResources(), overlay));
-
         rs.destroy();
+
+        view.setBackground(new BitmapDrawable(getResources(), overlay));
+        //模糊化结束后，才可以初始化布局
+        updateView();
+        Log.i("TAG", System.currentTimeMillis() - startMs + "ms");
     }
+
 
 }

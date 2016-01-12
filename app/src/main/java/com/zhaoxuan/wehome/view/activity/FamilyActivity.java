ac59eb2a34b5d0955f17349f98bbf08f9cdb74db
李兆轩 <690770333@qq.com>
2016-01-12 18:22:59 +0800

    no message

--- app/src/main/java/com/zhaoxuan/wehome/view/activity/FamilyActivity.java
+++ app/src/main/java/com/zhaoxuan/wehome/view/activity/FamilyActivity.java
@@ -1,16 +1,24 @@
 package com.zhaoxuan.wehome.view.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.zhaoxuan.wehome.R;
 import com.zhaoxuan.wehome.framework.base.BaseActivity;
 
+import butterknife.Bind;
+
 public class FamilyActivity extends BaseActivity {
+
+    @Bind(R.id.recyclerView)
+    protected RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+        
+
     }
 
     @Override
